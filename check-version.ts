a24edd0915ef334d97cc320cb8d591fda40f15a5
Alex Rattray <rattray.alex@gmail.com>
2022-09-20 15:45:43 +0100

    [internal] Improve check-version.ts

--- check-version.ts
+++ check-version.ts
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { VERSION } from './version';
 
 const main = () => {
   const pkg = JSON.parse(fs.readFileSync('package.json').toString()) as Record<string, unknown>;
@@ -8,13 +7,7 @@ const main = () => {
   if (typeof version !== 'string') {
     throw `Unexpected type for the package.json version field; got ${typeof version}, expected string`;
   }
-  if (version !== VERSION) {
-    throw (
-      `Version mismatch; package.json = ${version}, version.ts = ${VERSION}; ` +
-      `Please update the package.json version property or modify the VERSION variable in version.ts to ensure they match`
-    );
-  }
-  console.log(`Versions ${version} (package.json) and ${VERSION} (version.ts) match`);
+  fs.writeFileSync('version.ts', `export const VERSION = '${version}';\n`);
 };
 
 if (require.main === module) {
